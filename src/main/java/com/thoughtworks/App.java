42043280d30916fbca86bba3daec7807180248c6
duyuqian <15738381799@163.com>
2020-02-15 19:43:23 +0800

    [duyuqian] feat: create Student class with name&id&enrollmentDate,and override equals&toString method

--- src/main/java/com/thoughtworks/App.java
+++ src/main/java/com/thoughtworks/App.java
@@ -1,7 +1,11 @@
 package com.thoughtworks;
 
+import java.text.ParseException;
+
 public class App {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws ParseException {
+    Student student=new Student("xiaoming","001","2019.12.10");
+    System.out.println(student);
   }
 }
---
+++ src/main/java/com/thoughtworks/Student.java
@@ -0,0 +1,44 @@
+package com.thoughtworks;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Objects;
+
+public class Student {
+    private String name;
+    private String studentId;
+    private Date enrollmentDate;
+
+    public Student() {
+    }
+
+    public Student(String name, String studentId, String enrollmentDate) throws ParseException {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy.MM.dd");
+        this.name = name;
+        this.studentId = studentId;
+        this.enrollmentDate = simpleDateFormat.parse(enrollmentDate);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return Objects.equals(studentId, student.studentId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(studentId);
+    }
+
+    @Override
+    public String toString() {
+//        SimpleDateFormat simpleDateFormatOfYear = new SimpleDateFormat("yyyy");
+//        int schoolAge = Integer.parseInt(simpleDateFormatOfYear.format(new Date())) - Integer.parseInt(simpleDateFormatOfYear.format(enrollmentDate));
+        int schoolAge  = new Date().getYear()- enrollmentDate.getYear() ;
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy年M月dd日");
+        return "我叫" + name + "，我的学号是" + studentId + "，" + simpleDateFormat.format(enrollmentDate) + "入学，学龄" + schoolAge + "年";
+    }
+}
